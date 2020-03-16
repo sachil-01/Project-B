658b071a6cf0d3cb293b5602033b84ecb5eb68c0
Mohamad <0978573@hr.nl>
2020-03-16 20:39:38 +0100

    Start Scherm

---
+++ BrojectB/BrojectB9999/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace BrojectB9999
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panlStart = new System.Windows.Forms.Panel();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.picBox4 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlFilmInfo = new System.Windows.Forms.Panel();
+            this.labelSoortFilm = new System.Windows.Forms.Label();
+            this.btnBoken = new System.Windows.Forms.Button();
+            this.labelFilmDuurt = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picOnWard = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picLikeBoss = new System.Windows.Forms.PictureBox();
+            this.labelFilmInfo = new System.Windows.Forms.Label();
+            this.picPlus1 = new System.Windows.Forms.PictureBox();
+            this.picBlodsHot = new System.Windows.Forms.PictureBox();
+            this.picPlus = new System.Windows.Forms.PictureBox();
+            this.picNoTime = new System.Windows.Forms.PictureBox();
+            this.panlStart.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.pnlFilmInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOnWard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLikeBoss)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlus1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlodsHot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNoTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panlStart
+            // 
+            this.panlStart.Controls.Add(this.labelSearch);
+            this.panlStart.Controls.Add(this.textBox1);
+            this.panlStart.Controls.Add(this.picBox4);
+            this.panlStart.Controls.Add(this.picBox3);
+            this.panlStart.Controls.Add(this.picBox2);
+            this.panlStart.Controls.Add(this.picBox1);
+            this.panlStart.Location = new System.Drawing.Point(1, -2);
+            this.panlStart.Name = "panlStart";
+            this.panlStart.Size = new System.Drawing.Size(1048, 479);
+            this.panlStart.TabIndex = 0;
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Location = new System.Drawing.Point(750, 22);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(56, 17);
+            this.labelSearch.TabIndex = 7;
+            this.labelSearch.Text = "Zoeken";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(812, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // picBox4
+            // 
+            this.picBox4.Image = global::BrojectB9999.Properties.Resources._51it0D0mX2L__SX336_BO1_204_203_200_;
+            this.picBox4.Location = new System.Drawing.Point(825, 110);
+            this.picBox4.Name = "picBox4";
+            this.picBox4.Size = new System.Drawing.Size(163, 218);
+            this.picBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox4.TabIndex = 3;
+            this.picBox4.TabStop = false;
+            this.picBox4.Click += new System.EventHandler(this.picBox4_Click);
+            // 
+            // picBox3
+            // 
+            this.picBox3.Image = global::BrojectB9999.Properties.Resources._29419_121971_ps_sd_high;
+            this.picBox3.Location = new System.Drawing.Point(572, 110);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(172, 218);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox3.TabIndex = 2;
+            this.picBox3.TabStop = false;
+            this.picBox3.Click += new System.EventHandler(this.picBox3_Click);
+            // 
+            // picBox2
+            // 
+            this.picBox2.Image = global::BrojectB9999.Properties.Resources.maxresdefault;
+            this.picBox2.Location = new System.Drawing.Point(331, 110);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(163, 218);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 1;
+            this.picBox2.TabStop = false;
+            this.picBox2.Click += new System.EventHandler(this.picBox2_Click);
+            // 
+            // picBox1
+            // 
+            this.picBox1.Image = global::BrojectB9999.Properties.Resources.no_time_to_die;
+            this.picBox1.Location = new System.Drawing.Point(63, 110);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(169, 218);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            this.picBox1.Click += new System.EventHandler(this.picBox1_Click);
+            // 
+            // pnlFilmInfo
+            // 
+            this.pnlFilmInfo.Controls.Add(this.labelSoortFilm);
+            this.pnlFilmInfo.Controls.Add(this.btnBoken);
+            this.pnlFilmInfo.Controls.Add(this.labelFilmDuurt);
+            this.pnlFilmInfo.Controls.Add(this.pictureBox2);
+            this.pnlFilmInfo.Controls.Add(this.picOnWard);
+            this.pnlFilmInfo.Controls.Add(this.btnBack);
+            this.pnlFilmInfo.Controls.Add(this.pictureBox1);
+            this.pnlFilmInfo.Controls.Add(this.picLikeBoss);
+            this.pnlFilmInfo.Controls.Add(this.labelFilmInfo);
+            this.pnlFilmInfo.Controls.Add(this.picPlus1);
+            this.pnlFilmInfo.Controls.Add(this.picBlodsHot);
+            this.pnlFilmInfo.Controls.Add(this.picPlus);
+            this.pnlFilmInfo.Controls.Add(this.picNoTime);
+            this.pnlFilmInfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlFilmInfo.Name = "pnlFilmInfo";
+            this.pnlFilmInfo.Size = new System.Drawing.Size(1048, 479);
+            this.pnlFilmInfo.TabIndex = 4;
+            // 
+            // labelSoortFilm
+            // 
+            this.labelSoortFilm.AutoSize = true;
+            this.labelSoortFilm.Location = new System.Drawing.Point(16, 237);
+            this.labelSoortFilm.Name = "labelSoortFilm";
+            this.labelSoortFilm.Size = new System.Drawing.Size(46, 17);
+            this.labelSoortFilm.TabIndex = 12;
+            this.labelSoortFilm.Text = "label1";
+            // 
+            // btnBoken
+            // 
+            this.btnBoken.Location = new System.Drawing.Point(863, 421);
+            this.btnBoken.Name = "btnBoken";
+            this.btnBoken.Size = new System.Drawing.Size(126, 39);
+            this.btnBoken.TabIndex = 11;
+            this.btnBoken.Text = "Reserveren";
+            this.btnBoken.UseVisualStyleBackColor = true;
+            // 
+            // labelFilmDuurt
+            // 
+            this.labelFilmDuurt.AutoSize = true;
+            this.labelFilmDuurt.Location = new System.Drawing.Point(13, 216);
+            this.labelFilmDuurt.Name = "labelFilmDuurt";
+            this.labelFilmDuurt.Size = new System.Drawing.Size(38, 17);
+            this.labelFilmDuurt.TabIndex = 10;
+            this.labelFilmDuurt.Text = "lable";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BrojectB9999.Properties.Resources.depositphotos_12804673_stock_photo_eighteen_years_old_sign;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // picOnWard
+            // 
+            this.picOnWard.Image = global::BrojectB9999.Properties.Resources._51it0D0mX2L__SX336_BO1_204_203_200_;
+            this.picOnWard.Location = new System.Drawing.Point(0, -2);
+            this.picOnWard.Name = "picOnWard";
+            this.picOnWard.Size = new System.Drawing.Size(167, 211);
+            this.picOnWard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOnWard.TabIndex = 8;
+            this.picOnWard.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(28, 421);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(126, 39);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Terug";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BrojectB9999.Properties.Resources.depositphotos_12804673_stock_photo_eighteen_years_old_sign;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picLikeBoss
+            // 
+            this.picLikeBoss.Image = global::BrojectB9999.Properties.Resources._29419_121971_ps_sd_high;
+            this.picLikeBoss.Location = new System.Drawing.Point(0, 0);
+            this.picLikeBoss.Name = "picLikeBoss";
+            this.picLikeBoss.Size = new System.Drawing.Size(167, 209);
+            this.picLikeBoss.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLikeBoss.TabIndex = 5;
+            this.picLikeBoss.TabStop = false;
+            // 
+            // labelFilmInfo
+            // 
+            this.labelFilmInfo.AutoSize = true;
+            this.labelFilmInfo.Location = new System.Drawing.Point(328, 44);
+            this.labelFilmInfo.Name = "labelFilmInfo";
+            this.labelFilmInfo.Size = new System.Drawing.Size(46, 17);
+            this.labelFilmInfo.TabIndex = 0;
+            this.labelFilmInfo.Text = "label1";
+            // 
+            // picPlus1
+            // 
+            this.picPlus1.Image = global::BrojectB9999.Properties.Resources.depositphotos_12804673_stock_photo_eighteen_years_old_sign;
+            this.picPlus1.Location = new System.Drawing.Point(0, 158);
+            this.picPlus1.Name = "picPlus1";
+            this.picPlus1.Size = new System.Drawing.Size(45, 51);
+            this.picPlus1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlus1.TabIndex = 4;
+            this.picPlus1.TabStop = false;
+            // 
+            // picBlodsHot
+            // 
+            this.picBlodsHot.Image = global::BrojectB9999.Properties.Resources.maxresdefault;
+            this.picBlodsHot.Location = new System.Drawing.Point(0, -2);
+            this.picBlodsHot.Name = "picBlodsHot";
+            this.picBlodsHot.Size = new System.Drawing.Size(167, 211);
+            this.picBlodsHot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBlodsHot.TabIndex = 3;
+            this.picBlodsHot.TabStop = false;
+            // 
+            // picPlus
+            // 
+            this.picPlus.Image = global::BrojectB9999.Properties.Resources.depositphotos_12804673_stock_photo_eighteen_years_old_sign;
+            this.picPlus.Location = new System.Drawing.Point(1, 158);
+            this.picPlus.Name = "picPlus";
+            this.picPlus.Size = new System.Drawing.Size(45, 51);
+            this.picPlus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlus.TabIndex = 2;
+            this.picPlus.TabStop = false;
+            // 
+            // picNoTime
+            // 
+            this.picNoTime.Image = global::BrojectB9999.Properties.Resources.no_time_to_die;
+            this.picNoTime.Location = new System.Drawing.Point(0, 0);
+            this.picNoTime.Name = "picNoTime";
+            this.picNoTime.Size = new System.Drawing.Size(167, 209);
+            this.picNoTime.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picNoTime.TabIndex = 1;
+            this.picNoTime.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 489);
+            this.Controls.Add(this.pnlFilmInfo);
+            this.Controls.Add(this.panlStart);
+            this.Name = "Form1";
+            this.Text = "projectB";
+            this.panlStart.ResumeLayout(false);
+            this.panlStart.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.pnlFilmInfo.ResumeLayout(false);
+            this.pnlFilmInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOnWard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLikeBoss)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlus1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlodsHot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNoTime)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panlStart;
+        private System.Windows.Forms.PictureBox picBox4;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Panel pnlFilmInfo;
+        private System.Windows.Forms.Label labelFilmInfo;
+        private System.Windows.Forms.PictureBox picNoTime;
+        private System.Windows.Forms.PictureBox picPlus;
+        private System.Windows.Forms.PictureBox picBlodsHot;
+        private System.Windows.Forms.PictureBox picPlus1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picLikeBoss;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picOnWard;
+        private System.Windows.Forms.Label labelFilmDuurt;
+        private System.Windows.Forms.Button btnBoken;
+        private System.Windows.Forms.Label labelSoortFilm;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BrojectB/BrojectB9999/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrojectB9999
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panlStart.Show();
+            pnlFilmInfo.Hide();
+        }
+
+        private void picBox1_Click(object sender, EventArgs e)
+        {
+            
+            pnlFilmInfo.Show();
+            panlStart.Hide();
+            labelFilmInfo.Text = "In de 25e James Bond-film No Time to Die heeft James Bond zijn\n" +
+                " turbulente leven als geheim agent achter zich gelaten en leidt hij een rustig\n" +
+                " bestaan op Jamaica.\n" +
+                " Dit wordt abrupt onderbroken wanneer zijn oude vriend en CIA agent Felix Leiter\n" +
+                " hem vraagt om een ontvoerde wetenschapper te helpen bevrijden.\n" +
+                " Deze missie blijkt verraderlijker dan gedacht en brengt Bond op het spoor van een mysterieuze\n" +
+                " schurk die gewapend is met levensgevaarlijke nieuwe technologie.\n" +
+                " In No Time to Die keert Daniel Craig terug voor zijn vijfde film als Ian Flemings James Bond 007.\n" +
+                " De cast bestaat verder uit Rami Malek, Léa Seydoux, Lashana Lynch, Ben Whishaw, \n" +
+                "Naomie Harris, Billy Magnussen, Ana de Armas, Rory Kinnear, David Dencik, Dali Benssalah, \n" +
+                "Jeffrey Wright en Ralph Fiennes.\n" +
+                " Deze co-productie van Albert R. Broccoli’s EON Productions,\n" +
+                " Metro Goldwyn Mayer Studios (MGM) en Universal Pictures International is geregisseerd door\n" +
+                " Cary Joji Fukunaga (Beasts of No Nation, True Detective).\n" +
+                " Het scenario is geschreven door Neal Purvis & Robert Wade (Spectre, Skyfall), Cary Joji Fukunaga, \n" +
+                "Scott Z. Burns (Contagion, The Bourne Ultimatum) en Phoebe Waller-Bridge (Killing Eve, Fleabag)";
+            picNoTime.Show();
+            picPlus.Show();
+            picBlodsHot.Hide();
+            picLikeBoss.Hide();
+            picOnWard.Hide();
+            labelFilmDuurt.Text = "150 minuten";
+            labelSoortFilm.Text = "Actie, Avontuur, Thriller";
+        }
+
+        private void picBox2_Click(object sender, EventArgs e)
+        {
+            pnlFilmInfo.Show();
+            panlStart.Hide();
+            labelFilmInfo.Text = "In deze verfilming van de populaire gelijknamige comic speelt Vin Dies\n" +
+                "el de rol van Ray Garrison,\n een marinier die door de RTS Corporation opnieuw tot leven\n" +
+                " gewekt wordt als de superheld Bloodshot\n" +
+                "Met nanotechnologie in zijn aderen is hij sterker dan ooit en geneest hij meteen van \n" +
+                "verwondingen.\n De RTS Corporation heeft niet alleen de controle over zijn lichaam, \n" +
+                "maar ook over zijn geest en herinneringen.\nRay zet alles op het spel om de controle \n" +
+                "over zijn leven terug te krijgen.";
+            picNoTime.Hide();
+            picOnWard.Hide();
+
+            picBlodsHot.Show();
+            picLikeBoss.Hide();
+            labelFilmDuurt.Text = "130 minuten";
+            labelSoortFilm.Text = "Actie";
+        }
+
+        private void picBox3_Click(object sender, EventArgs e)
+        {
+            pnlFilmInfo.Show();
+            panlStart.Hide();
+            labelFilmInfo.Text = "In de hilarische nieuwe komedie Like a Boss lijkt het de beste\n" +
+                " vriendinnen Mia en Mel (Tiffany Haddish en Rose Byrne) voor de wind te gaan met hun \n" +
+                "eigen cosmeticabedrijf. Helaas zitten ze flink in de schulden en een aanbod van de \n" +
+                "keiharde cosmeticagigant Claire Luna (Salma Hayek) om de zaak over te kopen is dan \n" +
+                "ook te aanlokkelijk om af te slaan. De levenslange vriendschap tussen Mel en Mia komt \n" +
+                "hierna flink onder druk te staan.";
+            picNoTime.Hide();
+
+            picBlodsHot.Hide();
+            picLikeBoss.Show();
+            picOnWard.Hide();
+            labelFilmDuurt.Text = "160 minuten";
+            labelSoortFilm.Text = "Comedy";
+
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            pnlFilmInfo.Hide();
+            panlStart.Show();
+        }
+
+        private void picBox4_Click(object sender, EventArgs e)
+        {
+            pnlFilmInfo.Show();
+            panlStart.Hide();
+            labelFilmInfo.Text = "De nieuwe Disney en Pixar film Onward vertelt het verhaal van twee \n" +
+                "tiener elvenbroers. Samen beginnen ze aan een bijzondere en avontuurlijke zoektocht om" +
+                "\n te ontdekken of er nog ergens in de wereld magie te vinden is. \n" +
+                "Deze nieuwe animatiefilm van Pixar Animation Studios is geregisseerd door Dan Scanlon \n" +
+                "en geproduceerd door Kori Rae - het team achter Monsters University. Onward is vanaf 4\n" +
+                " maart 2020 te zien in de Nederlandse bioscopen. ";
+
+            picNoTime.Hide();
+            picBlodsHot.Hide();
+            picLikeBoss.Hide();
+            picOnWard.Show();
+            labelFilmDuurt.Text = "120 minuten";
+            labelSoortFilm.Text = "Animatie, Avontuur, Comedy, Kinderfilm";
+        }
+
+
+    }
+}
